9e5c0cf5798f5f194028fbd435e821d1628f7a2e
Ianyourgod <ianlouishawthorne@gmail.com>
2024-02-21 21:47:53 -0600

    add generateid function bc yes

--- api/db/UserManager.js
+++ api/db/UserManager.js
@@ -1,5 +1,16 @@
 const { MongoClient } = require('mongodb');
 
+function generateId() {
+    const rn = [
+        Math.random() * 100000,
+        Math.random() * 100000,
+        Math.random() * 100000,
+        Math.random() * 100000
+    ];
+    const raw = rn.join('.');
+    return Buffer.from(raw).toString("base64");
+};
+
 class UserManager {
     async init() {
         this.client = new MongoClient('mongodb://localhost:27017', { useUnifiedTopology: true });
