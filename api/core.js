0ba4015d984eaf23829482d2d484524bd9171d48
Ianyourgod <ianlouishawthorne@gmail.com>
2024-03-02 11:34:15 -0600

    get all that app.use shit in there

--- api/core.js
+++ api/core.js
@@ -1,8 +1,32 @@
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const rateLimit = require('express-rate-limit');
 const express = require("express");
 const v1Router = require("./v1/routes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors({
+    origin: '*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}));
+app.use(bodyParser.urlencoded({
+    limit: process.env.ServerSize,
+    extended: false
+}));
+app.set('trust proxy', 1);
+app.use(rateLimit({
+    validate: {
+        trustProxy: true,
+        xForwardedForHeader: true,
+    },
+    windowMs: 5000,  // 150 requests per 5 seconds
+    limit: 150,
+    standardHeaders: 'draft-7',
+    legacyHeaders: false,
+}));
 
 app.use("/api/v1", v1Router);
 
