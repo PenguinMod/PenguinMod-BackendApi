820ccda00befbf05ca8a0999568fa6e2b45ae796
Ianyourgod <ianlouishawthorne@gmail.com>
2024-03-06 07:57:11 -0600

    createAccount endpoint

---
+++ api/v1/routes/users/createAccount.js
@@ -0,0 +1,30 @@
+module.exports = (app, utils) => {
+    app.get("/api/v1/users/createAccount", async function (req, res) {
+        const packet = req.query;
+        if (!packet.username || !packet.password) {
+            utils.error(res, 400, "InvalidData");
+            return;
+        }
+
+        // TODO: this is currently pretty basic. we should add a few more requirements
+        if (packet.username.length < 3 || packet.username.length > 20) {
+            utils.error(res, 400, "InvalidData");
+            return;
+        }
+
+        if (packet.password.length < 6 || packet.password.length > 20) {
+            utils.error(res, 400, "InvalidData");
+            return;
+        }
+
+        if (await utils.UserManager.accountExists(packet.username)) {
+            utils.error(res, 400, "AccountExists");
+            return;
+        }
+
+        let token = await utils.UserManager.createAccount(packet.username, packet.password);
+        res.status(200);
+        res.header("Content-Type", 'application/json');
+        res.json({ "token": token });
+    });
+}
