e3eb39964f74c4f63dbe0bd277b85a54ff14b708
Ianyourgod <ianlouishawthorne@gmail.com>
2024-03-29 13:06:11 -0500

    its wokring i thinkw

--- api/v1/routes/users/OAuthlogin.js
+++ api/v1/routes/users/OAuthlogin.js
@@ -18,17 +18,21 @@ module.exports = (app, utils) => {
         // now make the request
         const response = await utils.UserManager.makeOAuth2Request(code, "scratch");
 
-        console.log(response.access_token);
-
         const username = await fetch("https://oauth2.scratch-wiki.info/w/rest.php/soa2/v0/user", {
             headers: {
                 Authorization: `Bearer ${btoa(response.access_token)}`
             }
-        }).then(res => res.status);
+        })
+        .then(async res => {
+            return {"user": await res.json(), "status": res.status};
+        });
 
-        console.log(username);
+        if (username.status !== 200) {
+            utils.error(res, 500, "InternalError");
+            return;
+        }
 
         res.status(200);
-        res.send("hi");
+        res.send(JSON.stringify(username));
     });
 }
