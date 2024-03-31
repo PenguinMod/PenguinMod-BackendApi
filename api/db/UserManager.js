6a8ffb5baf68f2ece7afb733e5d46de8bbbeab72
Ianyourgod <ianlouishawthorne@gmail.com>
2024-03-31 14:37:48 -0500

    i hate javascript

--- api/db/UserManager.js
+++ api/db/UserManager.js
@@ -716,12 +716,15 @@ class UserManager {
      * @async
      */
     async getProjectData(id) {
-        const result = await this.projects.findOne({id: id})
+        const tempresult = await this.projects.findOne({id: id})
 
         // add the views, loves, and votes
-        result.views = await this.getProjectViews(id);
-        result.loves = await this.getProjectLoves(id);
-        result.votes = await this.getProjectVotes(id);
+        const result = {
+            ...tempresult,
+            views: await this.getProjectViews(id),
+            loves: await this.getProjectLoves(id),
+            votes: await this.getProjectVotes(id)
+        }
 
         return result;
     }
