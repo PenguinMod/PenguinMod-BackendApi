0fb7ef1b231bbd0e36c8661c7e82cfdfd2572a3f
Ianyourgod <ianlouishawthorne@gmail.com>
2024-04-21 21:37:04 -0500

    change password + clean up

---
+++ api/v1/routes/users/login/changePassword.js
@@ -0,0 +1,37 @@
+module.exports = (app, utils) => {
+    app.post("/api/v1/users/changePassword", async function (req, res) {
+        const packet = req.body;
+
+        const username = packet.username;
+        const old_password = packet.old_password;
+        const new_password = packet.new_password;
+
+        if (!username || !old_password || !new_password) {
+            utils.error(res, 400, "InvalidData");
+            return;
+        }
+
+        if (typeof username !== "string" && typeof old_password !== "string" && typeof new_password !== "string") {
+            utils.error(res, 400, "InvalidData");
+            return;
+        }
+
+        if (new_password.length < 6 || new_password.length > 20) {
+            utils.error(res, 400, "InvalidData");
+            return;
+        }
+
+        if (!await utils.UserManager.loginWithPassword(username, old_password)) {
+            utils.error(res, 401, "InvalidData");
+            return;
+        }
+
+        await utils.UserManager.changePassword(username, new_password);
+
+        const token = await utils.UserManager.newTokenGen(username);
+
+        res.status(200);
+        res.header("Content-Type", 'application/json');
+        res.json({ "token": token });
+    });
+}
