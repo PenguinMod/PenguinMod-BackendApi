4345ce59415e7fedb9e9a21fe8299502885dfda6
Ianyourgod <ianlouishawthorne@gmail.com>
2024-04-28 14:52:49 -0500

    set pfp

---
+++ api/v1/routes/users/setPFP.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const Magic = require('mmmagic').Magic;
+const magic = new Magic();
+
+module.exports = (app, utils) => {
+    app.post('/api/v1/users/setPFP', utils.upload.single("picture"), async (req, res) => {
+        const packet = req.body;
+
+        const username = packet.username;
+        const token = packet.token;
+
+        const pictureName = req.file;
+
+        if (!await utils.UserManager.loginWithToken(username, token)) {
+            return utils.error(res, 401, "Invalid credentials");
+        }
+
+        if (!pictureName) {
+            return utils.error(res, 400, "No picture was provided");
+        }
+
+        const picture = fs.readFileSync(pictureName);
+
+        const allowedTypes = ["image/png", "image/jpeg"];
+
+        magic.detect(picture, async (err, result) => {
+            if (err) {
+                return utils.error(res, 400, "Invalid file type");
+            }
+
+            if (!allowedTypes.includes(result)) {
+                return utils.error(res, 400, "Invalid file type");
+            }
+
+            await utils.UserManager.setPFP(username, picture);
+
+            res.status(200);
+            res.header("Content-Type", 'application/json');
+            res.json({ "success": true });
+        });
+    });
+}
