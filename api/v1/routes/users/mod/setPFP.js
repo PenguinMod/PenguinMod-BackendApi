70e72dbc9d87def6a1c4f383123487d663032121
Ianyourgod <ianlouishawthorne@gmail.com>
2024-04-28 14:57:09 -0500

    set pfp mod

---
+++ api/v1/routes/users/mod/setPFP.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const Magic = require('mmmagic').Magic;
+const magic = new Magic();
+
+module.exports = (app, utils) => {
+    app.post('/api/v1/users/setPFPAdmin', utils.upload.single("picture"), async (req, res) => {
+        const packet = req.body;
+
+        const username = packet.username;
+        const token = packet.token;
+
+        const target = packet.target;
+
+        const pictureName = req.file;
+
+        if (!await utils.UserManager.loginWithToken(username, token)) {
+            return utils.error(res, 401, "Invalid credentials");
+        }
+
+        if (!await utils.UserManager.isAdmin(username) && !await utils.UserManager.isModerator(username)) {
+            return utils.error(res, 403, "FeatureDisabledForThisAccount");
+        }
+
+        if (!await utils.UserManager.existsByUsername(target)) {
+            return utils.error(res, 400, "InvalidData");
+        }
+
+        if (!pictureName) {
+            return utils.error(res, 400, "InvalidData");
+        }
+
+        const picture = fs.readFileSync(pictureName);
+
+        const allowedTypes = ["image/png", "image/jpeg"];
+
+        magic.detect(picture, async (err, result) => {
+            if (err) {
+                return utils.error(res, 400, "Invalid file type");
+            }
+
+            if (!allowedTypes.includes(result)) {
+                return utils.error(res, 400, "Invalid file type");
+            }
+
+            await utils.UserManager.setPFP(target, picture);
+
+            res.status(200);
+            res.header("Content-Type", 'application/json');
+            res.json({ "success": true });
+        });
+    });
+}
