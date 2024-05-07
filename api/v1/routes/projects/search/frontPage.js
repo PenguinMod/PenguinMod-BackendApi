be27731d2988d55420ba181951eed8b92b23645a
Ianyourgod <ianlouishawthorne@gmail.com>
2024-05-07 08:28:42 -0500

    fix

--- api/v1/routes/projects/search/frontPage.js
+++ api/v1/routes/projects/search/frontPage.js
@@ -32,30 +32,30 @@ module.exports = (app, utils) => {
 
         const tag = tags[Math.floor(Math.random() * tags.length)];
 
-        const featured = await utils.projects.getFeaturedProjects(0, Number(utils.env.PageSize));
-        const almostFeatured = await utils.projects.specializedSearch(
+        const featured = await utils.UserManager.getFeaturedProjects(0, Number(utils.env.PageSize));
+        const almostFeatured = await utils.UserManager.specializedSearch(
             { $match: { featured: false, },
               $gte: { votes: utils.env.FeatureAmount - 5 }
             },
             0,
             Number(utils.env.PageSize)
         );
-        const liked = await utils.projects.specializedSearch(
+        const liked = await utils.UserManager.specializedSearch(
             { $match: { featured: false, },
               $gte: { votes: 5 }
             },
             0,
             Number(utils.env.PageSize)
         );
-        const highViews = await utils.projects.specializedSearch(
+        const highViews = await utils.UserManager.specializedSearch(
             { $match: { featured: false, },
               $gte: { views: 30 }
             },
             0,
             Number(utils.env.PageSize)
         );
-        const fitsTags = await utils.projects.searchForTag(tag, 0, Number(utils.env.PageSize));
-        const latest = await utils.projects.getProjects(0, Number(utils.env.PageSize));
+        const fitsTags = await utils.UserManager.searchForTag(tag, 0, Number(utils.env.PageSize));
+        const latest = await utils.UserManager.getProjects(0, Number(utils.env.PageSize));
 
         res.header("Content-Type", "application/json");
         res.status(200);
