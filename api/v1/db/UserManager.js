e8518a6c1de9611586fde04878ae64c9e312b95f
Ianyourgod <ianlouishawthorne@gmail.com>
2024-06-02 21:24:08 -0500

    change base pfp

--- api/v1/db/UserManager.js
+++ api/v1/db/UserManager.js
@@ -10,7 +10,7 @@ const fs = require('fs');
 const path = require('path');
 var prompt = require('prompt-sync')();
 
-const basePFP = fs.readFileSync(path.join(__dirname, "./scratcher.png"));
+const basePFP = fs.readFileSync(path.join(__dirname, "./penguin.png"));
 
 class UserManager {
     static loginInvalidationTime = 
