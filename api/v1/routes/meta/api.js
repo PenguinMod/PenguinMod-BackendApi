01f0e9c861398b5c89d1d0310cd453982ad9c6cf
Ianyourgod <ianlouishawthorne@gmail.com>
2024-06-05 22:34:14 -0500

    update api metadata

--- api/v1/routes/meta/api.js
+++ api/v1/routes/meta/api.js
@@ -1,9 +1,18 @@
+const fs = require('fs');
 const path = require('path');
+const child_process = require('child_process');
 
 module.exports = function(app, utils) {
     app.get("/api/v1/", (req, res) => {
-        res.status(200);
-        res.header("Content-Type", 'application/json');
-        res.sendFile(path.join(utils.homeDir, './metadata.json'));
+        // read the json file
+        const metadata = JSON.parse(fs.readFileSync(path.join(utils.homeDir, './metadata.json'), 'utf8'));
+
+        child_process.exec('git rev-parse HEAD', function(err, stdout) {
+            metadata.version.git = stdout.trim();
+
+            res.status(200);
+            res.header("Content-Type", 'application/json');
+            res.json(metadata);
+        });
     });
 }
