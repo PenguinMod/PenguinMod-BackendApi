aa6199449c94b28146519be67c4d26ae90b682fc
Ianyourgod <104151459+Ianyourgod@users.noreply.github.com>
2024-07-17 01:41:59 -0500

    more testing

--- api/v1/routes/misc/updateApi.js
+++ api/v1/routes/misc/updateApi.js
@@ -3,15 +3,20 @@ const path = require("path");
 
 module.exports = (app, utils) => {
     app.post("/api/v1/misc/updateApi", async (req, res) => {
-        let packet = req.body;
+        try {
+            let packet = req.body;
 
-        // get headers
-        const headers = req.headers;
+            // get headers
+            const headers = req.headers;
 
-        packet.headers = headers;
+            packet.headers = headers;
 
-        // write packet and headers to file
-        fs.writeFileSync(path.join(utils.HomeDir, "apiPacket.json"), JSON.stringify(packet, null, 4));
+            // write packet and headers to file
+            fs.writeFileSync(path.join(utils.HomeDir, "apiPacket.json"), JSON.stringify(packet, null, 4));
+        } catch (e) {
+            // return e
+            return utils.error(res, 500, e); 
+        }
 
         res.status(200);
         res.header("Content-Type", "application/json");
