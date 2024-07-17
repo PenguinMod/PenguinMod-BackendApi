424ac8c3f326962c1bb1a72b8d0c20a07a02583a
Ianyourgod <104151459+Ianyourgod@users.noreply.github.com>
2024-07-17 02:57:36 -0500

    more tests

--- api/v1/routes/misc/updateApi.js
+++ api/v1/routes/misc/updateApi.js
@@ -13,8 +13,10 @@ module.exports = (app, utils) => {
         // write packet and headers to file
         fs.writeFileSync(path.join(utils.homeDir, "apiPacket.json"), JSON.stringify(packet, null, 4));
 
+        const fileExists = fs.existsSync(path.join(utils.homeDir, "apiPacket.json"));
+
         res.status(200);
         res.header("Content-Type", "application/json");
-        res.send({ success: true });
+        res.send({ success: true, fileExists: fileExists, path: path.join(utils.homeDir, "apiPacket.json") });
     });
 } 
