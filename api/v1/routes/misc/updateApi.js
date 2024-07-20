c56d2f37728da917d4cc32445cc275cb28e41fa7
Ianyourgod <104151459+Ianyourgod@users.noreply.github.com>
2024-07-20 00:56:19 -0500

    mmmmmmmmmmmmmmm

--- api/v1/routes/misc/updateApi.js
+++ api/v1/routes/misc/updateApi.js
@@ -62,7 +62,7 @@ module.exports = (app, utils) => {
 
         const verified = await verifySignature(utils.env.ReloadApiKey, providedHash, JSON.stringify(packet));
 
-        utils.log.sendServerLog(`Verified: ${verified}`, 0x11c195);
+        utils.log.sendServerLog(`Verified: ${verified}`, 0x11c195); 
 
         if (!verified) {
             res.sendStatus(400);
