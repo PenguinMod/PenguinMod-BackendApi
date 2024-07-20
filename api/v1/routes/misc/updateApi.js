115afc45c43f497e3126d85e1c55a0fdb17a8f33
Ianyourgod <104151459+Ianyourgod@users.noreply.github.com>
2024-07-20 00:34:20 -0500

    update api

--- api/v1/routes/misc/updateApi.js
+++ api/v1/routes/misc/updateApi.js
@@ -1,4 +1,49 @@
-const { createHash, timingSafeEqual } = require("crypto");
+const crypto = require("crypto");
+
+let encoder = new TextEncoder();
+
+async function verifySignature(secret, header, payload) {
+    let parts = header.split("=");
+    let sigHex = parts[1];
+
+    let algorithm = { name: "HMAC", hash: { name: 'SHA-256' } };
+
+    let keyBytes = encoder.encode(secret);
+    let extractable = false;
+    let key = await crypto.subtle.importKey(
+        "raw",
+        keyBytes,
+        algorithm,
+        extractable,
+        [ "sign", "verify" ],
+    );
+
+    let sigBytes = hexToBytes(sigHex);
+    let dataBytes = encoder.encode(payload);
+    let equal = await crypto.subtle.verify(
+        algorithm.name,
+        key,
+        sigBytes,
+        dataBytes,
+    );
+
+    return equal;
+}
+
+function hexToBytes(hex) {
+    let len = hex.length / 2;
+    let bytes = new Uint8Array(len);
+
+    let index = 0;
+    for (let i = 0; i < hex.length; i += 2) {
+        let c = hex.slice(i, i + 2);
+        let b = parseInt(c, 16);
+        bytes[index] = b;
+        index += 1;
+    }
+
+    return bytes;
+}
 
 module.exports = (app, utils) => {
     if (utils.env.IncludeReload !== "true") {
@@ -8,13 +53,17 @@ module.exports = (app, utils) => {
         const packet = req.body;
         const headers = req.headers;
 
-        const secretHash = "sha256=".concat(createHash("sha256").update(utils.env.ReloadApiKey).update(JSON.stringify(packet)).digest("hex"));
         const providedHash = headers["x-hub-signature-256"];
 
-        utils.logs.sendServerLog(`in server: ${secretHash}\nsent: ${providedHash}`, 0x11c195);
+        if (!providedHash) {
+            res.sendStatus(400);
+            return;
+        }
 
-        if (!timingSafeEqual(Buffer.from(secretHash), Buffer.from(providedHash))) {
-            res.sendStatus(401);
+        const verified = await verifySignature(utils.env.GithubWebhookSecret, providedHash, JSON.stringify(packet));
+
+        if (!verified) {
+            res.sendStatus(400);
             return;
         }
 
