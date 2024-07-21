76272ae54528e0ee13d2b7e3bfbdedf2fb8154c1
Ianyourgod <104151459+Ianyourgod@users.noreply.github.com>
2024-07-21 00:29:52 -0500

    make removeOAuthMethod a post

--- api/v1/routes/users/oauth/removeMethod/removeOAuthMethod.js
+++ api/v1/routes/users/oauth/removeMethod/removeOAuthMethod.js
@@ -1,7 +1,7 @@
 module.exports = (app, utils) => {
-    app.get("/api/v1/users/removeoauthmethod", async function (req, res) {
+    app.post("/api/v1/users/removeoauthmethod", async function (req, res) {
         // get the method
-        const packet = req.query;
+        const packet = req.body;
 
         const method = packet.method;
         const username = (String(packet.username)).toLowerCase();
@@ -25,5 +25,9 @@ module.exports = (app, utils) => {
         }
 
         await utils.UserManager.removeOAuthMethod(username, method);
+
+        res.status(200);
+        res.header("Content-Type", 'application/json');
+        res.send({ "success": true });
     });
 }
