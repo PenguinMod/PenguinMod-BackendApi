78bb1a66604f85a736992fef855e0809fe120d0a
Ianyourgod <104151459+Ianyourgod@users.noreply.github.com>
2024-10-16 20:18:14 -0500

    Update shutUpLittleKids.js

--- api/v1/routes/misc/shutUpLittleKids.js
+++ api/v1/routes/misc/shutUpLittleKids.js
@@ -2,7 +2,7 @@
 
 module.exports = (app, utils) => {
     app.get('/api/users/login', async (req, res) => {
-        res.status(200);
-        res.text("The server is down, meaning you cannot login. Please check the status page or the discord server for more information.");
+        res.status(403);
+        res.send("The server is down, meaning you cannot login. Please check the status page or the discord server for more information.");
     });
 }
