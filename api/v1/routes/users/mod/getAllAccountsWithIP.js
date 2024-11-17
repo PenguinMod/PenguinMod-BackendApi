6b5790fde2fd85ba7e99eced9c3ca0ccb793a05d
sussy layers dev <72522395+jwklong@users.noreply.github.com>
2024-11-17 16:33:03 +0000

    getAllAccountsWithIP endpoint

---
+++ api/v1/routes/users/mod/getAllAccountsWithIP.js
@@ -0,0 +1,24 @@
+module.exports = (app, utils) => {
+    app.get("/api/v1/users/getAllAccountsWithIP", utils.cors(), async function (req, res) {
+        const packet = req.query;
+
+        const username = (String(packet.username)).toLowerCase();
+        const token = packet.token;
+
+        const target = packet.target;
+
+        if (!await utils.UserManager.loginWithToken(username, token)) {
+            return utils.error(res, 401, "Reauthenticate");
+        }
+
+        if (!await utils.UserManager.isAdmin(username)) {
+            return utils.error(res, 403, "Unauthorized");
+        }
+
+        const users = await utils.UserManager.getAllAccountsWithIP(target);
+
+        res.status(200);
+        res.header("Content-Type", 'application/json');
+        res.json({ users });
+    });
+}
