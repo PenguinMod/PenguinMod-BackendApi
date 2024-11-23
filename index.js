843cde53eb559b0115f96abe442bf5c2bafb0be0
Ianyourgod <104151459+Ianyourgod@users.noreply.github.com>
2024-11-22 23:05:22 -0600

    mmm

--- index.js
+++ index.js
@@ -63,8 +63,8 @@ app.use(rateLimit({
         trustProxy: true,
         xForwardedForHeader: true,
     },
-    windowMs: 5000,  // 20 requests per 5 seconds
-    limit: 20,
+    windowMs: 5000,  // 100 requests per 5 seconds
+    limit: 100,
     standardHeaders: 'draft-7',
     legacyHeaders: false,
 }));
