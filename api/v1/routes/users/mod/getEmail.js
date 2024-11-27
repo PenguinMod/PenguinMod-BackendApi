8802bcab4e4ba0dd2ddc6db2eba43e5616a21529
Ianyourgod <104151459+Ianyourgod@users.noreply.github.com>
2024-11-26 18:31:34 -0600

    get email

---
+++ api/v1/routes/users/mod/getEmail.js
@@ -0,0 +1,32 @@
+module.exports = (app, utils) => {
+    app.get('/api/v1/users/getemail', utils.cors(), async function (req, res) {
+        const packet = req.query;
+
+        const username = (String(packet.username)).toLowerCase();
+        const token = packet.token;
+
+        const target = (String(packet.target)).toLowerCase();
+
+        if (!username || !token) {
+            return utils.error(res, 400, "Missing username or token");
+        }
+
+        if (!await utils.UserManager.loginWithToken(username, token)) {
+            return utils.error(res, 401, "Invalid credentials");
+        }
+
+        if (!await utils.UserManager.isAdmin(username)) {
+            return utils.error(res, 401, "Unauthorized");
+        }
+
+        const data = await utils.UserManager.getUserData(target);
+
+        if (!data) {
+            return utils.error(res, 404, "UserNotFound");
+        }
+
+        res.status(200);
+        res.header('Content-type', "application/json");
+        res.send({ email: data.email });
+    });
+}
