13f1babfa889a2ce46ee9459a4453020afab3b43
Ianyourgod <104151459+Ianyourgod@users.noreply.github.com>
2024-12-03 21:23:43 -0600

    be more sigma

--- api/v1/routes/projects/search/getRandom.js
+++ api/v1/routes/projects/search/getRandom.js
@@ -4,6 +4,6 @@ module.exports = (app, utils) => {
 
         res.status(200);
         res.header("Content-Type", 'application/json');
-        return res.send(project);
+        return res.send(project || {});
     });
 }
