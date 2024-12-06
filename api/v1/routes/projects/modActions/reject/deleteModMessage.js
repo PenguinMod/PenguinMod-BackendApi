5d4ce9a7f9297f57124509cb7683ed4ddacc87d4
Ianyourgod <104151459+Ianyourgod@users.noreply.github.com>
2024-12-06 00:30:18 -0600

    delete mod message

---
+++ api/v1/routes/projects/modActions/reject/deleteModMessage.js
@@ -0,0 +1,44 @@
+module.exports = (app, utils) => {
+    app.post('/api/v1/projects/deletemodmessage', utils.cors(), async (req, res) => {
+        const packet = req.body;
+
+        const username = (String(packet.username)).toLowerCase();
+        const token = packet.token;
+
+        const messageID = packet.messageID;
+
+        if (!username || !token || typeof messageID !== "string") {
+            return utils.error(res, 400, "Missing username, token, target, or message");
+        }
+
+        if (!await utils.UserManager.loginWithToken(username, token)) {
+            return utils.error(res, 401, "Invalid credentials");
+        }
+
+        if (!await utils.UserManager.isAdmin(username) && !await utils.UserManager.isModerator(username)) {
+            return utils.error(res, 401, "Invalid credentials");
+        }
+
+        utils.logs.sendAdminLog(
+            {
+                action: `Mod message has been deleted`,
+                content: `${username} deleted mod message ${messageID}`,
+                fields: [
+                    {
+                        name: "Mod",
+                        value: username
+                    },
+                    {
+                        name: "Message Content",
+                        value: (await utils.UserManager.getMessage(messageID)).message
+                    }
+                ]
+            }
+        );
+
+        await utils.UserManager.deleteMessage(messageID);
+
+        res.header('Content-type', "application/json");
+        res.send({ success: true });
+    });
+}
