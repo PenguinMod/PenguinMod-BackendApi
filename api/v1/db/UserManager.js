451d09f42ff8ad7acb253eea614f54ec409f0e16
Ianyourgod <104151459+Ianyourgod@users.noreply.github.com>
2025-04-12 10:41:18 -0500

    why must js be stupid

--- api/v1/db/UserManager.js
+++ api/v1/db/UserManager.js
@@ -3705,7 +3705,7 @@ class UserManager {
         // check if remix is not 0
         const remixCount = await this.projects.countDocuments({ remix: { $ne: "0" } });
         const featuredCount = await this.projects.countDocuments({ featured: true });
-        const totalViews = await this.projects.aggregate([{$group: {_id:null, total_views:{$sum:"$views"}}}]).toArray()[0].total_views;
+        const totalViews = (await this.projects.aggregate([{$group: {_id:null, total_views:{$sum:"$views"}}}]).toArray()).at(0).total_views;
 
         const mongodb_stats = await this.db.command(
             {
