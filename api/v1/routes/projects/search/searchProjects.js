bf43ace9e5e17d72c06233e8e3c8211064c02717
Ianyourgod <104151459+Ianyourgod@users.noreply.github.com>
2025-04-21 00:17:57 -0500

    edit that too

--- api/v1/routes/projects/search/searchProjects.js
+++ api/v1/routes/projects/search/searchProjects.js
@@ -5,13 +5,14 @@ module.exports = (app, utils) => {
         const query = packet.query || "";
         const page = utils.handle_page(packet.page);
         const type = packet.type || "";
+        const reverse = packet.reverse || false;
 
         const username = packet.username;
         const token = packet.token;
 
         const is_mod = username && token && await utils.UserManager.loginWithToken(username, token) && await utils.UserManager.isModeratorOrAdmin(username);
 
-        const projects = await utils.UserManager.searchProjects(is_mod, query, type, page, Number(utils.env.PageSize));
+        const projects = await utils.UserManager.searchProjects(is_mod, query, type, page, Number(utils.env.PageSize), reverse);
 
         res.status(200);
         res.header({"Content-Type": "application/json"})
