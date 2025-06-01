1b6c3136a9bf409eda917121d78957309cb4527a
Ianyourgod <104151459+Ianyourgod@users.noreply.github.com>
2025-06-01 11:21:25 -0500

    fix private accounts bug

--- api/v1/routes/projects/search/getProjectsByAuthor.js
+++ api/v1/routes/projects/search/getProjectsByAuthor.js
@@ -2,6 +2,9 @@ module.exports = (app, utils) => {
     app.get('/api/v1/projects/getprojectsbyauthor', async (req, res) => {
         const packet = req.query;
 
+        const username = String(packet.username).toLowerCase();
+        const token = packet.token;
+
         const authorUsername = String(packet.authorUsername).toLowerCase();
         const page = utils.handle_page(packet.page);
 
@@ -13,7 +16,29 @@ module.exports = (app, utils) => {
             return utils.error(res, 404, "UserNotFound")
         }
 
-        const id = await utils.UserManager.getIDByUsername(authorUsername);
+        const logged_in = username && token && await utils.UserManager.loginWithToken(username, token);
+        const is_author = logged_in && username == authorUsername;
+
+        const target_data = await utils.UserManager.getUserData(target);
+
+        const id = target_data.id;
+        const privateProfile = target_data.privateProfile;
+        const canFollowingSeeProfile = target_data.allowFollowingView;
+
+        if (privateProfile) {
+            if (!logged_in) {
+                await utils.error(res, 403, "PrivateProfile");
+            }
+
+            const user_id = await utils.UserManager.getIDByUsername(username);
+            const is_following = await utils.UserManager.isFollowing(id, user_id);
+
+            if (username !== target && (
+                !(is_following && canFollowingSeeProfile) && !isMod
+            )) {
+                await utils.error(res, 403, "PrivateProfile");
+            }
+        }
 
         const projects = (await utils.UserManager.getProjectsByAuthor(id, page, Number(utils.env.PageSize)))
         .map(project => {
