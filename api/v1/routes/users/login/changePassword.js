48bd87767418e0e1bbbcd5804d1805c06001e23e
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2025-08-09 03:59:32 -0600

    Update changePassword.js

--- api/v1/routes/users/login/changePassword.js
+++ api/v1/routes/users/login/changePassword.js
@@ -14,25 +14,41 @@ module.exports = (app, utils) => {
     app.post("/api/v1/users/changePassword", utils.cors(), async function (req, res) {
         const packet = req.body;
 
-        const username = (String(packet.username)).toLowerCase();
+        const token = packet.token;
+        if (typeof token !== "string") {
+            utils.error(res, 400, "Missing token");
+            return;
+        }
+
         const old_password = packet.old_password;
         const new_password = packet.new_password;
-
-        if (!username || !old_password || !new_password) {
-            utils.error(res, 400, "Missing username, old_password, or new_password");
+        if (!old_password || !new_password) {
+            utils.error(res, 400, "Missing old_password or new_password");
+            return;
+        }
+        if (typeof old_password !== "string" && typeof new_password !== "string") {
+            utils.error(res, 400, "Missing old_password or new_password");
+            return;
+        }
+        const passwordDoesNotMeetLength = new_password.length < 8 || new_password.length > 50;
+        const passwordMeetsTextInclude = new_password.match(/[a-z]/) && new_password.match(/[A-Z]/);
+        const passwordMeetsSpecialInclude = new_password.match(/[0-9]/) && new_password.match(/[^a-z0-9]/i);
+        if (passwordDoesNotMeetLength) {
+            utils.error(res, 400, "InvalidLengthPassword");
+            return;
+        }
+        if (!(passwordMeetsTextInclude && passwordMeetsSpecialInclude)) {
+            utils.error(res, 400, "MissingRequirementsPassword");
             return;
         }
 
-        if (typeof username !== "string" && typeof old_password !== "string" && typeof new_password !== "string") {
-            utils.error(res, 400, "Missing username, old_password, or new_password");
+        const login = await utils.UserManager.loginWithToken(token);
+        if (!login.success) {
+            utils.error(res, 400, "Reauthenticate");
             return;
         }
 
-        if (new_password.length < 6 || new_password.length > 20) {
-            utils.error(res, 400, "InvalidPassword");
-            return;
-        }
-
+        const username = login.username;
         if (!await utils.UserManager.loginWithPassword(username, old_password)) {
             utils.error(res, 401, "Invalid Login");
             return;
@@ -40,12 +56,12 @@ module.exports = (app, utils) => {
 
         await utils.UserManager.changePassword(username, new_password);
 
-        const token = await utils.UserManager.newTokenGen(username);
+        const newToken = await utils.UserManager.newTokenGen(username);
 
         await utils.UserManager.addIP(username, req.realIP);
 
         res.status(200);
         res.header("Content-Type", 'application/json');
-        res.json({ "token": token });
+        res.json({ "token": newToken });
     });
 }
