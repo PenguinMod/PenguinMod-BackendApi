9fc121b1eaee9cd7eb0b7a22c10e5f4e11d4282a
ianyourgod <ianyourgod@gmail.com>
2026-03-21 15:38:22 -0500

    delete report if its invalid

--- api/v1/routes/misc/reports/getReports.js
+++ api/v1/routes/misc/reports/getReports.js
@@ -6,7 +6,7 @@ const UserManager = require("../../../db/UserManager");
  */
 
 /**
- * 
+ *
  * @param {any} app Express app
  * @param {Utils} utils Utils
  */
@@ -18,42 +18,56 @@ module.exports = (app, utils) => {
 
         const login = await utils.UserManager.loginWithToken(token);
         if (!login.success) {
-            utils.error(res, 401, "Reauthenticate")
+            utils.error(res, 401, "Reauthenticate");
             return;
         }
         const username = login.username;
 
-        if (!await utils.UserManager.hasModPerms(username)) {
+        if (!(await utils.UserManager.hasModPerms(username))) {
             return utils.error(res, 403, "Unauthorized");
         }
 
         const type = packet.type;
         const page = utils.handle_page(packet.page);
 
-        const reports = type ?
-                        await utils.UserManager.getReportsByType(type, page, Number(utils.env.PageSize)) :
-                        await utils.UserManager.getReports(page, Number(utils.env.PageSize))
-        
+        const reports = type
+            ? await utils.UserManager.getReportsByType(
+                  type,
+                  page,
+                  Number(utils.env.PageSize),
+              )
+            : await utils.UserManager.getReports(
+                  page,
+                  Number(utils.env.PageSize),
+              );
+
         const final = [];
 
         for (const report of reports) {
             let pushing = {
-                reporter: await utils.UserManager.getUsernameByID(report.reporter),
+                reporter: await utils.UserManager.getUsernameByID(
+                    report.reporter,
+                ),
                 target: report.reportee,
                 targetID: report.reportee,
                 report: report.reason,
                 author: "",
                 date: report.date,
                 type: report.type,
-                id: report.id
-            }
+                id: report.id,
+            };
 
             if (pushing.type === "user") {
-                pushing.target = await utils.UserManager.getUsernameByID(pushing.targetID);
+                pushing.target = await utils.UserManager.getUsernameByID(
+                    pushing.targetID,
+                );
             } else {
-                const metadata = await utils.UserManager.getProjectMetadata(pushing.targetID);
+                const metadata = await utils.UserManager.getProjectMetadata(
+                    pushing.targetID,
+                );
 
                 if (!metadata) {
+                    await utils.UserManager.deleteReport(pushing.id);
                     continue;
                 }
 
@@ -63,9 +77,9 @@ module.exports = (app, utils) => {
 
             final.push(pushing);
         }
-        
+
         res.status(200);
         res.header("Content-Type", "application/json");
         res.send({ reports: final });
     });
-}
+};
