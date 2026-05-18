1f0d89deb1b7031e71e2f4886d3904b044d611e0
ianyourgod <ianyourgod@gmail.com>
2026-05-17 22:42:16 -0500

    format

--- api/v1/routes/users/meta/customization/setCustomization.js
+++ api/v1/routes/users/meta/customization/setCustomization.js
@@ -6,53 +6,70 @@ const UserManager = require("../../../../db/UserManager");
  */
 
 /**
- * 
+ *
  * @param {any} app Express app
  * @param {Utils} utils Utils
  */
 module.exports = (app, utils) => {
-    app.post("/api/v1/users/customization/setCustomization", utils.cors(), async (req, res) => {
-        const packet = req.body;
+    app.post(
+        "/api/v1/users/customization/setCustomization",
+        utils.cors(),
+        async (req, res) => {
+            const packet = req.body;
 
-        const token = packet.token;
-        const customization = packet.customization;
-        
-        if (!token || !customization || typeof(customization) !== "object") {
-            utils.error(res, 400, "Missing token or customization");
-            return;
-        }
+            const token = packet.token;
+            const customization = packet.customization;
 
-        const login = await utils.UserManager.loginWithToken(token);
-        if (!login.success) {
-            utils.error(res, 400, "Reauthenticate");
-            return;
-        }
-        const username = login.username;
-
-        // you can change other people's customization if you are a mod
-        const target = packet.target ? String(packet.target).toLowerCase() : null;
-        if (target && !await utils.UserManager.isModeratorOrAdmin(username)) {
-            return utils.error(res, 401, "Invalid credentials");
-        }
-        if (!target) {
-            const badges = await utils.UserManager.getBadges(username);
-
-            if (!badges.includes("donator")) {
-                utils.error(res, 403, "MissingPermission");
+            if (!token || !customization || typeof customization !== "object") {
+                utils.error(res, 400, "Missing token or customization");
                 return;
             }
-            if (await utils.UserManager.getUserCustomizationDisabled(username)) {
-                utils.error(res, 403, "FeatureDisabledForThisAccount");
+
+            const login = await utils.UserManager.loginWithToken(token);
+            if (!login.success) {
+                utils.error(res, 400, "Reauthenticate");
                 return;
             }
-        }
+            const username = login.username;
 
-        const errorReason = utils.UserManager.verifyCustomData(customization);
-        if (errorReason) return utils.error(res, 400, errorReason);
-        await utils.UserManager.setUserCustomization(target || username, customization);
+            // you can change other people's customization if you are a mod
+            const target = packet.target
+                ? String(packet.target).toLowerCase()
+                : null;
+            if (
+                target &&
+                !(await utils.UserManager.isModeratorOrAdmin(username))
+            ) {
+                return utils.error(res, 401, "Invalid credentials");
+            }
+            if (!target) {
+                const badges = await utils.UserManager.getBadges(username);
 
-        res.status(200);
-        res.header("Content-Type", "application/json");
-        res.send({ success: true });
-    });
-}
+                if (!badges.includes("donator")) {
+                    utils.error(res, 403, "MissingPermission");
+                    return;
+                }
+                if (
+                    await utils.UserManager.getUserCustomizationDisabled(
+                        username,
+                    )
+                ) {
+                    utils.error(res, 403, "FeatureDisabledForThisAccount");
+                    return;
+                }
+            }
+
+            const errorReason =
+                utils.UserManager.verifyCustomData(customization);
+            if (errorReason) return utils.error(res, 400, errorReason);
+            await utils.UserManager.setUserCustomization(
+                target || username,
+                customization,
+            );
+
+            res.status(200);
+            res.header("Content-Type", "application/json");
+            res.send({ success: true });
+        },
+    );
+};
